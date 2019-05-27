e2320807787379841361d2cca8edbc9340d3316e
Vlad Moisescu <vladm@intern.pch>
2019-05-27 15:01:04 +0300

    make it better

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -17,18 +17,18 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
 
-    var connectionString = "postgres://fmvczwhersemte:46ab7ff9cca6264621ce1a69f3658ae12691027d8838c4a82c20f8801313a3f9@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d78tcc2bsd8nkq"
+    //var connectionString = "postgres://fmvczwhersemte:46ab7ff9cca6264621ce1a69f3658ae12691027d8838c4a82c20f8801313a3f9@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d78tcc2bsd8nkq"
 
-    // Client.connect(connectionString, function(err, client, done) {
-    //     console.log(err+"!!!!!!!!!!!!!!!");
-    //    client.query('SELECT * FROM users', function(err, result) {
-    //      done();
-    //      if(err) return console.error(err);
-    //      console.log(result.rows);
-    //    });
-    //  });
+    Client.connect(process.env.DATABASE_URL, function(err, client, done) {
+        console.log(err+"!!!!!!!!!!!!!!!");
+       client.query('SELECT * FROM users', function(err, result) {
+         done();
+         if(err) return console.error(err);
+         console.log(result.rows);
+       });
+     });
 
-    res.send(process.env.DATABASE_URL);
+    //res.send(process.env.DATABASE_URL);
 
 });
 
