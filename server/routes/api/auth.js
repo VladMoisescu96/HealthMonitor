75505830649124394579d56bab2d838c43d4a944
Vlad Moisescu <vladm@intern.pch>
2019-05-27 12:54:35 +0300

    make it better

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -22,6 +22,13 @@ async function loadUsers() {
     });
       
     client.connect();
+
+    client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+          console.log(JSON.stringify(row));
+        }
+
     
     return client;
 }
