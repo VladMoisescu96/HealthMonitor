fe5468fa1e89de87f4ff91bba6a655bf8eca8698
Vlad Moisescu <vladm@intern.pch>
2019-05-27 15:36:06 +0300

    make it better

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -9,8 +9,6 @@ const client = new Client({
   ssl: true,
 });
 
-//client.connect();
-
 router.get('/', async (req, res) => {
     res.send("Hello from users");
 });
@@ -20,6 +18,8 @@ router.post('/', async (req, res) => {
     //var connectionString = "postgres://fmvczwhersemte:46ab7ff9cca6264621ce1a69f3658ae12691027d8838c4a82c20f8801313a3f9@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d78tcc2bsd8nkq"
 
     client.connect();
+    client.query('INSERT into users (username, email, password) VALUES($1, $2, $3) RETURNING id', 
+    [req.body.username, req.body.email, req.body.password]);
 
     res.send(process.env.DATABASE_URL);
 
