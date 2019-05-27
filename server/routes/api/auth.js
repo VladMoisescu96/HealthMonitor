df4ce654703452a5520fcd62d09ddbe9a2e0fb76
Vlad Moisescu <vladm@intern.pch>
2019-05-27 14:52:48 +0300

    make it better

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -17,9 +17,11 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
 
-    Client.connect(process.env.DATABASE_URL, function(err, client, done) {
+    var connectionString = "postgres://fmvczwhersemte:46ab7ff9cca6264621ce1a69f3658ae12691027d8838c4a82c20f8801313a3f9@ec2-46-137-187-23.eu-west-1.compute.amazonaws.com:5432/d78tcc2bsd8nkq"
+
+    Client.connect(connectionString, function(err, client, done) {
         console.log(err+"!!!!!!!!!!!!!!!");
-       client.query('SELECT * FROM your_table', function(err, result) {
+       client.query('SELECT * FROM users', function(err, result) {
          done();
          if(err) return console.error(err);
          console.log(result.rows);
