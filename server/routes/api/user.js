6b6b75314cd81762a923902a56a7c8bd398dde88
Vlad Moisescu <vladm@intern.pch>
2019-05-29 12:32:20 +0300

    make it better

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -2,17 +2,17 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+const authMiddleware = (req, res, next) => {  
+    if (!req.isAuthenticated()) {
+        res.status(401).send('You are not authenticated')
+    } else {
+        return next()
+    }
+}
+
 router.get("/", authMiddleware, (req, res) => {  
   
     res.send({ user: 7 })
   });
 
-  const authMiddleware = (req, res, next) => {  
-    if (!req.isAuthenticated()) {
-      res.status(401).send('You are not authenticated')
-    } else {
-      return next()
-    }
-  }
-
-  module.exports = router;
+module.exports = router;
