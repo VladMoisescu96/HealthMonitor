f7a99eba975990614801bea98963362c2aea52b5
Vlad Moisescu <vladm@intern.pch>
2019-05-29 17:01:36 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -51,15 +51,16 @@ passport.use(
 
             }
 
-            if (result.rows[0].password == password) {
-                user = { username: result.rows[0].username,
-                         password: result.rows[0].password
-                        }
+            done(null, user);
+            // if (result.rows[0].password == password) {
+            //     user = { username: result.rows[0].username,
+            //              password: result.rows[0].password
+            //             }
                 
-                done(null, user);
-            } else {
-                done(null, false, {message: 'Incorrect username or password'})
-            }
+            //     done(null, user);
+            // } else {
+            //     done(null, false, {message: 'Incorrect username or password'})
+            // }
 
         });
        })
